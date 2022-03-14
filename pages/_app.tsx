73afb0dbd448531aae37cf272f586e19cd7e58b4
jonah <jonah@wichtrup-li.de>
2022-03-14 20:57:32 +0100

    update environments

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -30,7 +30,7 @@ export default function App(props: AppProps) {
         <>
             <Head>
                 <title>Mantine next example</title>
-                <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+                <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no" />
             </Head>
 
             <ColorSchemeProvider
